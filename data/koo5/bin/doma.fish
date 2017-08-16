8c2e99764df00c83bbf2e107469481217e1fb713
koo5 <jindrich.kolman@elektroline.cz>
2017-08-16 15:49:06 +0200

    stuff

--- data/koo5/bin/doma.fish
+++ data/koo5/bin/doma.fish
@@ -1,3 +1,3 @@
 #!/usr/bin/env fish
-./snaploop.py 555 /home/kook /home/snapshots&
-sudo -u kook ./screenshot-whole-loop.py /home/kook/screenshots
+./snaploop.py 555 /home/kook /home/snapshots
+#sudo -u kook ./screenshot-whole-loop.py /home/kook/screenshots
--- data/koo5/bin/pause_resume_program.py
+++ data/koo5/bin/pause_resume_program.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import sys
 import subprocess
-program = 'firefox'
+if len(sys.argv) == 1:
+	program = 'firefox'
+else:
+	program = sys.argv[1]
 if len(sys.argv) == 2:
 	program = sys.argv[1]
 x =  subprocess.check_output("ps -l -A | grep " + program, shell = True).decode()
